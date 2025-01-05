cd10dd68094f7d9436f25418855d9ec5c6e77723
papauri <johnpaulchirwa@gmail.com>
2025-01-06 10:33:20 +1100

    1st commit
    My First commit
    
    Docker Set  up
    
    Firebase Set up

---
+++ frontend/scripts/main.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyClJfGFoc1WZ_qYi5ImQJXyurQtqXgOqfA",
+  authDomain: "banknkonde.firebaseapp.com",
+  databaseURL: "https://banknkonde-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "banknkonde",
+  storageBucket: "banknkonde.appspot.com",
+  messagingSenderId: "698749180404",
+  appId: "1:698749180404:web:7e8483cae4abd7555101a1",
+  measurementId: "G-MC7PDS90FR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
